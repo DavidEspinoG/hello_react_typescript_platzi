efeb1a11e7143a49c3d7a8ff01653b4e6688f9dc
David Espino <davidespinog@gmail.com>
2023-08-23 11:09:30 -0600

    Add click listener to fox images

--- app/components/RandomFox.tsx
+++ app/components/RandomFox.tsx
@@ -3,11 +3,10 @@ type Props = {
 };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-
   return <img 
     width={320} 
     height='auto' 
     src={image}
-    className="rounded"
+    className="rounded mb-6"
     />
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
+'use client';
 import { RandomFox } from "./components/RandomFox"
+import { MouseEventHandler, useState } from "react";
+
+type ImageItem = {id: string, url: string}
+
+const random = (): number => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
-  const random = (): number => Math.floor(Math.random() * 123) + 1;
+  const generateId = Math.random().toString(36).substring(2,9);
+  const [ images, setImages ] = useState<ImageItem[]>([
+    {id: 'a', url: `https://randomfox.ca/images/${random()}.jpg`},
+  ]);
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+    const newItem: ImageItem = {
+      id: generateId, 
+      url:`https://randomfox.ca/images/${random()}.jpg`
+    }
+    setImages([...images, newItem ]);
+  };
+
   return (
     <main>
       <h1>Hello TypeScript</h1>
-      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
+      <button
+        onClick={addNewFox}
+      >
+        Add new fox
+      </button>
+      {images.map((item, index) => (
+        <div key={index}>
+        <RandomFox 
+          image={item.url}
+        />
+        </div>
+      ))}
+      
     </main>
   )
 }
