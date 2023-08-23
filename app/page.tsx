8eaf0b068ff47ae592d88adc728bcf043a077353
David Espino <davidespinog@gmail.com>
2023-08-23 10:09:56 -0600

    Add RandomFox component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { RandomFox } from "./components/RandomFox"
 
 export default function Home() {
+  const random = (): number => Math.floor(Math.random() * 123) + 1;
   return (
     <main>
       <h1>Hello TypeScript</h1>
+      <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`}/>
     </main>
   )
 }
